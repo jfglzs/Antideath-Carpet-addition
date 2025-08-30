979013f0a4cca0d72d20b28417f37393774eb3bd
antideath <243911689@qq.com>
2025-08-30 17:21:14 +0800

    remove worldTime command

--- src/main/java/io/jfglzs/ad_carpet_addition/commands/IEntityPlayerActionPackAction.java
+++
@@ -1,8 +0,0 @@
-package io.jfglzs.ad_carpet_addition.commands;
-
-public interface IEntityPlayerActionPackAction
-{
-    void setWorldTime(long WorldTime);
-}
-
-//TIS carpet addition https://github.com/TISUnion/Carpet-TIS-Addition Fallen_Breath
